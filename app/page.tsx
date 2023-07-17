73ddb10d082163df9816ded126d822d68d1c02b5
Swapnull <martyn.e.rushton@gmail.com>
2023-07-17 12:41:13 +0100

    feat(teamDetail): Add team detail page and clean up Table component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { orderBy } from "lodash";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 import Table from "./shared/components/Table";
+import { getLogoByTeamName } from "./shared/constants/logo";
+import { Team } from "~/keelClient";
 
 const keelClient = getKeelServer();
 
@@ -14,8 +16,6 @@ export default async function Home() {
     ? orderBy(teams?.data?.results, ["name"], ["asc"])
     : [];
 
-  console.log(teams);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <TeamList teams={orderedTeams} />
@@ -23,79 +23,16 @@ export default async function Home() {
   );
 }
 
-const TeamLogos = {
-  "Atlanta Hawks":
-    "https://upload.wikimedia.org/wikipedia/en/2/24/Atlanta_Hawks_logo.svg",
-  "Boston Celtics":
-    "https://upload.wikimedia.org/wikipedia/en/8/8f/Boston_Celtics.svg",
-  "Brooklyn Nets":
-    "https://upload.wikimedia.org/wikipedia/commons/4/44/Brooklyn_Nets_newlogo.svg",
-  "Charlotte Hornets":
-    "https://upload.wikimedia.org/wikipedia/en/c/c4/Charlotte_Hornets_%282014%29.svg",
-  "Chicago Bulls":
-    "https://upload.wikimedia.org/wikipedia/en/6/67/Chicago_Bulls_logo.svg",
-  "Cleveland Cavaliers":
-    "https://upload.wikimedia.org/wikipedia/commons/4/4b/Cleveland_Cavaliers_logo.svg",
-  "Dallas Mavericks":
-    "https://upload.wikimedia.org/wikipedia/en/9/97/Dallas_Mavericks_logo.svg",
-  "Denver Nuggets":
-    "https://upload.wikimedia.org/wikipedia/en/7/76/Denver_Nuggets.svg",
-  "Detroit Pistons":
-    "https://upload.wikimedia.org/wikipedia/commons/c/c9/Logo_of_the_Detroit_Pistons.svg",
-  "Golden State Warriors":
-    "https://upload.wikimedia.org/wikipedia/en/0/01/Golden_State_Warriors_logo.svg",
-  "Houston Rockets":
-    "https://upload.wikimedia.org/wikipedia/en/2/28/Houston_Rockets.svg",
-  "Indiana Pacers":
-    "https://upload.wikimedia.org/wikipedia/en/1/1b/Indiana_Pacers.svg",
-  "LA Clippers":
-    "https://upload.wikimedia.org/wikipedia/en/b/bb/Los_Angeles_Clippers_%282015%29.svg",
-  "Los Angeles Lakers":
-    "https://upload.wikimedia.org/wikipedia/commons/3/3c/Los_Angeles_Lakers_logo.svg",
-  "Memphis Grizzlies":
-    "https://upload.wikimedia.org/wikipedia/en/f/f1/Memphis_Grizzlies.svg",
-  "Miami Heat":
-    "https://upload.wikimedia.org/wikipedia/en/f/fb/Miami_Heat_logo.svg",
-  "Milwaukee Bucks":
-    "https://upload.wikimedia.org/wikipedia/en/4/4a/Milwaukee_Bucks_logo.svg",
-  "Minnesota Timberwolves":
-    "https://upload.wikimedia.org/wikipedia/en/c/c2/Minnesota_Timberwolves_logo.svg",
-  "New Orleans Pelicans":
-    "https://upload.wikimedia.org/wikipedia/en/0/0d/New_Orleans_Pelicans_logo.svg",
-  "New York Knicks":
-    "https://upload.wikimedia.org/wikipedia/en/2/25/New_York_Knicks_logo.svg",
-  "Oklahoma City Thunder":
-    "https://upload.wikimedia.org/wikipedia/en/5/5d/Oklahoma_City_Thunder.svg",
-  "Orlando Magic":
-    "https://upload.wikimedia.org/wikipedia/en/1/10/Orlando_Magic_logo.svg",
-  "Philadelphia 76ers":
-    "https://upload.wikimedia.org/wikipedia/en/0/0e/Philadelphia_76ers_logo.svg",
-  "Phoenix Suns":
-    "https://upload.wikimedia.org/wikipedia/en/d/dc/Phoenix_Suns_logo.svg",
-  "Portland Trail Blazers":
-    "https://upload.wikimedia.org/wikipedia/commons/3/33/Portland-Trail-Blazers-Logo-2002.png",
-  "Sacramento Kings":
-    "https://upload.wikimedia.org/wikipedia/en/c/c7/SacramentoKings.svg",
-  "San Antonio Spurs":
-    "https://upload.wikimedia.org/wikipedia/en/a/a2/San_Antonio_Spurs.svg",
-  "Toronto Raptors":
-    "https://upload.wikimedia.org/wikipedia/en/3/36/Toronto_Raptors_logo.svg",
-  "Utah Jazz":
-    "https://upload.wikimedia.org/wikipedia/en/0/04/Utah_Jazz_logo_%282016%29.svg",
-  "Washington Wizards":
-    "https://upload.wikimedia.org/wikipedia/en/0/02/Washington_Wizards_logo.svg",
-};
-
 function TeamList({ teams }) {
-  const headers = ["Name", "Next Game", "Last Game", "Record"];
   const columns = [
     {
-      children: (info) => (
+      header: "Name",
+      getBody: (info: Team) => (
         <div className="flex items-center">
           <div className="flex-shrink-0 w-10 h-10">
             <Image
               className="w-full h-full rounded-full"
-              src={TeamLogos[info.name]}
+              src={getLogoByTeamName(info.name)}
               alt=""
               width={40}
               height={40}
@@ -106,13 +43,16 @@ function TeamList({ teams }) {
       ),
     },
     {
-      children: (info) => <div>Next game</div>,
+      header: "Next game",
+      getBody: (info: Team) => <div>Next game</div>,
     },
     {
-      children: (info) => <div>Last game</div>,
+      header: "Last game",
+      getBody: (info: Team) => <div>Last game</div>,
     },
     {
-      children: (info) => <div>Record</div>,
+      header: "Record",
+      getBody: (info: Team) => <div>Record</div>,
     },
   ];
 
@@ -124,7 +64,6 @@ function TeamList({ teams }) {
             <Table
               data={teams}
               columns={columns}
-              headers={headers}
               rowLink={(row) => `team/${row.id}`}
             />
           </div>
--- app/team/[id]/page.tsx
+++ app/team/[id]/page.tsx
@@ -1,6 +1,71 @@
+import Table from "~/app/shared/components/Table";
+import { getKeelServer } from "../../keel/server";
+import { DateTime } from "luxon";
+import { Player } from "~/keelClient";
+
 interface TeamPageProps {
   params: { id: string };
 }
-export default function TeamPage({ params }: TeamPageProps) {
-  return <div>{params.id}</div>;
+
+const keelClient = getKeelServer();
+
+export default async function TeamPage({ params }: TeamPageProps) {
+  const { data: team, error } = await keelClient.getTeam({ id: params.id });
+  const { data: playerList } = await keelClient.getPlayersOnTeam({
+    teamId: params.id,
+  });
+
+  if (error || !team) return <div>Team not found</div>;
+
+  const columns = [
+    {
+      header: "Name",
+      getBody: (player: Player) => `${player.firstName} ${player.lastName}`,
+    },
+    {
+      header: "Number",
+      getBody: (player: Player) => player.jersey,
+    },
+    {
+      header: "Position",
+      getBody: (player: Player) => player.position,
+    },
+    {
+      header: "DoB",
+      getBody: (player: Player) =>
+        DateTime.fromISO(player.birthDate).toFormat("dd MMM yyyy"),
+    },
+    {
+      header: "Height",
+      getBody: (player: Player) =>
+        `${Math.floor(player.height / 12)}'${Math.floor(player.height % 12)}"`,
+    },
+    {
+      header: "Weight",
+      getBody: (player: Player) => `${player.weight} lbs`,
+    },
+    {
+      header: "College",
+      getBody: (player: Player) => player.college,
+    },
+  ];
+
+  return (
+    <div className="p-4">
+      <h1 className="text-xl">{team.name}</h1>
+      {playerList?.players?.length ? (
+        <Table
+          data={playerList?.players}
+          columns={columns}
+          rowLink={(info) => `/player/${info.id}`}
+          orderBy={(player) => {
+            if (!player.jersey) return 999;
+            return Number(player.jersey);
+          }}
+        />
+      ) : (
+        <div> No players </div>
+      )}
+    </div>
+  );
 }
